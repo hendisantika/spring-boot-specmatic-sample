ce87535ec904e7016df27a4fc1b5c9aafcb97c38
hendisantika <hendisantika@yahoo.co.id>
2025-07-02 07:52:24 +0700

    feat: Add GlobalExceptionHandler🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/handler/GlobalExceptionHandler.kt
@@ -0,0 +1,17 @@
+package id.my.hendisantika.specmaticsample.handler
+
+import org.springframework.web.bind.annotation.ControllerAdvice
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-specmatic-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 02/07/25
+ * Time: 07.52
+ * To change this template use File | Settings | File Templates.
+ */
+@ControllerAdvice
+class GlobalExceptionHandler
