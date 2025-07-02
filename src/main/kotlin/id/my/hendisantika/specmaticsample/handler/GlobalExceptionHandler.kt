c24201d863d2046532696624478adfc8444cb360
hendisantika <hendisantika@yahoo.co.id>
2025-07-02 07:52:42 +0700

    feat: Add handleGenericException in GlobalExceptionHandler🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/specmaticsample/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/handler/GlobalExceptionHandler.kt
@@ -1,6 +1,11 @@
 package id.my.hendisantika.specmaticsample.handler
 
+import id.my.hendisantika.specmaticsample.exception.NotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.ErrorResponse
 import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,4 +19,17 @@ import org.springframework.web.bind.annotation.ControllerAdvice
  * To change this template use File | Settings | File Templates.
  */
 @ControllerAdvice
-class GlobalExceptionHandler
+class GlobalExceptionHandler {
+    @ExceptionHandler(NotFoundException::class)
+    fun handleGenericException(ex: NotFoundException): ResponseEntity<ErrorResponse> {
+        val notFound = HttpStatus.NOT_FOUND
+        return ResponseEntity.status(notFound).body(
+            errorResponse(
+                notFound,
+                ex,
+                "Requested resource not found",
+                "resource not found"
+            )
+        )
+    }
+}
