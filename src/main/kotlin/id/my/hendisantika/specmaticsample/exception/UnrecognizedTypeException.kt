ab9608601ed1a374109b7d4d8cc1f5f146b9101b
hendisantika <hendisantika@yahoo.co.id>
2025-07-02 07:50:38 +0700

    feat: Add UnrecognizedTypeException🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/exception/UnrecognizedTypeException.kt
@@ -0,0 +1,18 @@
+package id.my.hendisantika.specmaticsample.exception
+
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-specmatic-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 02/07/25
+ * Time: 07.50
+ * To change this template use File | Settings | File Templates.
+ */
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+class UnrecognizedTypeException(type: String) : Throwable("Unrecognized type: $type")
