f911711ca7f9349b8bc2d82fc290b68156fab4a1
hendisantika <hendisantika@yahoo.co.id>
2025-07-04 06:02:29 +0700

    feat: Add ProductService🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/service/ProductService.kt
@@ -0,0 +1,17 @@
+package id.my.hendisantika.specmaticsample.service
+
+import org.springframework.stereotype.Service
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-specmatic-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 04/07/25
+ * Time: 06.02
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+class ProductService
