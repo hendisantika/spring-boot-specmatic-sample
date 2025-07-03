dc9eaeed8afd9a7f47bc62fbabd94e870b7dc686
hendisantika <hendisantika@yahoo.co.id>
2025-07-04 05:54:57 +0700

    feat: Add Order🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/model/Order.kt
@@ -0,0 +1,26 @@
+package id.my.hendisantika.specmaticsample.model
+
+import jakarta.validation.constraints.Positive
+import java.util.concurrent.atomic.AtomicInteger
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-specmatic-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 04/07/25
+ * Time: 05.54
+ * To change this template use File | Settings | File Templates.
+ */
+class Order(
+    @field:Positive val productid: Int = 0,
+    @field:Positive val count: Int = 0,
+    @field:NotNull var status: OrderStatus = OrderStatus.pending,
+    val id: Int = idGenerator.getAndIncrement()
+) {
+    companion object {
+        val idGenerator: AtomicInteger = AtomicInteger(1)
+    }
+}
