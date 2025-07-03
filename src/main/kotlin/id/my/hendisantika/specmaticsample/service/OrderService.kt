c9f527df3a59f4e23c4526021ae1b5881ce671d9
hendisantika <hendisantika@yahoo.co.id>
2025-07-04 06:00:14 +0700

    feat: Add OrderService🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/service/OrderService.kt
@@ -0,0 +1,17 @@
+package id.my.hendisantika.specmaticsample.service
+
+import org.springframework.stereotype.Service
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-specmatic-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 04/07/25
+ * Time: 05.59
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+class OrderService
