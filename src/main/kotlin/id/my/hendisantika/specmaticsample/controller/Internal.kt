a40b9e04557ef9f1666ba6059044bded5540749c
hendisantika <hendisantika@yahoo.co.id>
2025-07-04 06:04:56 +0700

    feat: Add metrics in Internal Controller🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/specmaticsample/controller/Internal.kt
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/controller/Internal.kt
@@ -2,6 +2,7 @@ package id.my.hendisantika.specmaticsample.controller
 
 import id.my.hendisantika.specmaticsample.service.MetricService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -19,4 +20,9 @@ import org.springframework.web.bind.annotation.RestController
 class Internal {
     @Autowired
     lateinit var metricService: MetricService
+
+    @GetMapping("/internal/metrics")
+    fun metrics(): Int {
+        return metricService.activeUsers()
+    }
 }
