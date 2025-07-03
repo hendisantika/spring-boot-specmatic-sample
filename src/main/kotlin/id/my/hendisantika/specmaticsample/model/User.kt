0c816871ecee5e39f30343ccea1976ee5fc22575
hendisantika <hendisantika@yahoo.co.id>
2025-07-04 05:56:52 +0700

    feat: Add User Data class🫸🌀✏️📗🐧🐳

---
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/model/User.kt
@@ -0,0 +1,14 @@
+package id.my.hendisantika.specmaticsample.model
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-specmatic-sample
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 04/07/25
+ * Time: 05.56
+ * To change this template use File | Settings | File Templates.
+ */
+data class User(val name: String)
