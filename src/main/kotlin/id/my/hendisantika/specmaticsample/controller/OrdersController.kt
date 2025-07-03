bba06757a862340b6b8350eb6ae02fcec2923c89
hendisantika <hendisantika@yahoo.co.id>
2025-07-04 06:05:59 +0700

    feat: Add create in OrdersController🫸🌀✏️📗🐧🐳

--- src/main/kotlin/id/my/hendisantika/specmaticsample/controller/OrdersController.kt
+++ src/main/kotlin/id/my/hendisantika/specmaticsample/controller/OrdersController.kt
@@ -1,7 +1,16 @@
 package id.my.hendisantika.specmaticsample.controller
 
+import id.my.hendisantika.specmaticsample.model.Id
+import id.my.hendisantika.specmaticsample.model.Order
+import id.my.hendisantika.specmaticsample.model.User
 import id.my.hendisantika.specmaticsample.service.OrderService
+import jakarta.validation.Valid
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 /**
@@ -19,4 +28,12 @@ import org.springframework.web.bind.annotation.RestController
 class OrdersController {
     @Autowired
     lateinit var orderService: OrderService
+
+    @PostMapping("/orders")
+    fun create(@Valid @RequestBody order: Order, @AuthenticationPrincipal user: User): ResponseEntity<Id> {
+        println(order.status.toString())
+        val orderId = orderService.createOrder(order)
+        return ResponseEntity(orderId, HttpStatus.OK)
+    }
+
 }
